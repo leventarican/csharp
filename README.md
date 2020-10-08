77f9f8e02f3a9cc4a14365573d3e60ab78a12d00
leventarican <levent@protonmail.com>
2020-10-08 23:32:11 +0200

    .NET 5

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
-# C# .NET Core
-* get donet from https://dotnet.microsoft.com/download
+# .NET 5.0
+* _.NET 5_ is the next release after _.NET core_ 3.1. _.NET Framework_ will not receive any further release.
+* .NET is a fusion of core and framework, or just the next release of core
+
+## install .NET 5 under linux
+* go to https://dotnet.microsoft.com/download/dotnet/5.0
+* download `x64` binary
+* decompress and add dotnet folder to your path
+```
+mkdir dotnet
+tar zxf dotnet-sdk-5.0.100-rc.1.20452.10-linux-x64.tar.gz -C dotnet/
+
+PATH="$HOME/development/dotnet:$PATH"
+export DOTNET_ROOT=$HOME/development/dotnet
+```
+* check installation
+```
+dotnet --list-sdks
+5.0.100-rc.1.20452.10 [/home/code/development/dotnet/sdk]
+```
+
+## create console app
+```
+dotnet new console --output cli
+```
+
+## run app 
+```
+dotnet run --project cli
+dotnet run
+```
+
+
+## links
+* target frameworks: https://docs.microsoft.com/en-us/dotnet/standard/frameworks#supported-target-frameworks
+* evolution of .NET Core to .NET 5: https://docs.microsoft.com/en-us/dotnet/core/dotnet-five
+> .NET 5, which is the next release of .NET Core following 3.1
+
+# .NET core 3.1
 * create project: `dotnet new web -n microservice`
     * folder `microservice` will be created
 
---
+++ cli/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace cli
+{
+    class Program
+    {
+        // class members / fields
+        const int Readonly = 100;
+        string status = "ready";
+
+        public Program()
+        {
+            status = "constructed";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("now: " + now.ToString());
+
+            int[] a = new int[10];
+            a[5] = 100;
+            int[] b = { 20, 30, 40 };
+
+            foreach (int bb in b)
+            {
+                Console.WriteLine("b: " + bb);
+            }
+
+            Program program = new Program();
+            Console.WriteLine("program status: " + program.status);
+            Console.WriteLine("program readonly value: " + Program.Readonly);
+
+            Developer dev = new Developer();
+            dev.ProgrammingLanguage = "java";   // set
+            Console.WriteLine("get property: " + dev.ProgrammingLanguage);
+            dev.Experience = 10;
+            Console.WriteLine("get property: " + dev.Experience);
+        }
+    }
+
+    class Developer
+    {
+        // field 
+        private string programmingLanguage;
+        // property
+        public string ProgrammingLanguage
+        {
+            get { return programmingLanguage; }
+            set { programmingLanguage = value; }
+        }
+
+        // short hand (automatic property)
+        public int Experience
+        {
+            get; set;
+        }
+    }
+
+    class CloudDeveloper : Developer {
+        public string publiccloud = "abc cloud";
+    }
+
+    sealed class SealedClass  {
+        public string inherit = "this class cant be inherited";
+    }
+}
